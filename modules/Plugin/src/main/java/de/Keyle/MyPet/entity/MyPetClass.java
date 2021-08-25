4cf1039b66e12ac5161bc51de69d9813f4f1f1cb
cobrex1 <44735826+cobrex1@users.noreply.github.com>
2021-08-25 17:23:23 +0100

    Added GlowSquid

--- modules/Plugin/src/main/java/de/Keyle/MyPet/entity/MyPetClass.java
+++ modules/Plugin/src/main/java/de/Keyle/MyPet/entity/MyPetClass.java
@@ -28,103 +28,104 @@ import de.Keyle.MyPet.entity.types.*;
 import java.lang.reflect.Constructor;
 
 public enum MyPetClass {
-    Axolotl(MyAxolotl.class),
-    Bat(MyBat.class),
-    Bee(MyBee.class),
-    Blaze(MyBlaze.class),
-    Cat(MyCat.class),
-    CaveSpider(MyCaveSpider.class),
-    Chicken(MyChicken.class),
-    Cod(MyCod.class),
-    Cow(MyCow.class),
-    Creeper(MyCreeper.class),
-    Dolphin(MyDolphin.class),
-    Donkey(MyDonkey.class),
-    Drowned(MyDrowned.class),
-    ElderGuardian(MyElderGuardian.class),
-    EnderDragon(MyEnderDragon.class),
-    Enderman(MyEnderman.class),
-    Endermite(MyEndermite.class),
-    Evoker(MyEvoker.class),
-    Fox(MyFox.class),
-    Ghast(MyGhast.class),
-    Goat(MyGoat.class),
-    Giant(MyGiant.class),
-    Guardian(MyGuardian.class),
-    Hoglin(MyHoglin.class),
-    Horse(MyHorse.class),
-    Husk(MyHusk.class),
-    Illusioner(MyIllusioner.class),
-    IronGolem(MyIronGolem.class),
-    Llama(MyLlama.class),
-    MagmaCube(MyMagmaCube.class),
-    Mooshroom(MyMooshroom.class),
-    Mule(MyMule.class),
-    Ocelot(MyOcelot.class),
-    Panda(MyPanda.class),
-    Parrot(MyParrot.class),
-    Phantom(MyPhantom.class),
-    Pig(MyPig.class),
-    Piglin(MyPiglin.class),
-    PiglinBrute(MyPiglinBrute.class),
-    PigZombie(MyPigZombie.class),
-    Pillager(MyPillager.class),
-    PolarBear(MyPolarBear.class),
-    Pufferfish(MyPufferfish.class),
-    Rabbit(MyRabbit.class),
-    Ravager(MyRavager.class),
-    Salmon(MySalmon.class),
-    Sheep(MySheep.class),
-    Silverfish(MySilverfish.class),
-    Skeleton(MySkeleton.class),
-    SkeletonHorse(MySkeletonHorse.class),
-    Slime(MySlime.class),
-    Snowman(MySnowman.class),
-    Spider(MySpider.class),
-    Squid(MySquid.class),
-    Stray(MyStray.class),
-    Strider(MyStrider.class),
-    TraderLlama(MyTraderLlama.class),
-    TropicalFish(MyTropicalFish.class),
-    Turtle(MyTurtle.class),
-    WanderingTrader(MyWanderingTrader.class),
-    Witch(MyWitch.class),
-    Wither(MyWither.class),
-    WitherSkeleton(MyWitherSkeleton.class),
-    Wolf(MyWolf.class),
-    Vex(MyVex.class),
-    Villager(MyVillager.class),
-    Vindicator(MyVindicator.class),
-    Zoglin(MyZoglin.class),
-    Zombie(MyZombie.class),
-    ZombieHorse(MyZombieHorse.class),
-    ZombieVillager(MyZombieVillager.class),
-    ZombifiedPiglin(MyZombifiedPiglin.class);
+	Axolotl(MyAxolotl.class),
+	Bat(MyBat.class),
+	Bee(MyBee.class),
+	Blaze(MyBlaze.class),
+	Cat(MyCat.class),
+	CaveSpider(MyCaveSpider.class),
+	Chicken(MyChicken.class),
+	Cod(MyCod.class),
+	Cow(MyCow.class),
+	Creeper(MyCreeper.class),
+	Dolphin(MyDolphin.class),
+	Donkey(MyDonkey.class),
+	Drowned(MyDrowned.class),
+	ElderGuardian(MyElderGuardian.class),
+	EnderDragon(MyEnderDragon.class),
+	Enderman(MyEnderman.class),
+	Endermite(MyEndermite.class),
+	Evoker(MyEvoker.class),
+	Fox(MyFox.class),
+	Ghast(MyGhast.class),
+	Giant(MyGiant.class),
+	GlowSquid(MyAxolotl.class),
+	Goat(MyGoat.class),
+	Guardian(MyGuardian.class),
+	Hoglin(MyHoglin.class),
+	Horse(MyHorse.class),
+	Husk(MyHusk.class),
+	Illusioner(MyIllusioner.class),
+	IronGolem(MyIronGolem.class),
+	Llama(MyLlama.class),
+	MagmaCube(MyMagmaCube.class),
+	Mooshroom(MyMooshroom.class),
+	Mule(MyMule.class),
+	Ocelot(MyOcelot.class),
+	Panda(MyPanda.class),
+	Parrot(MyParrot.class),
+	Phantom(MyPhantom.class),
+	Pig(MyPig.class),
+	Piglin(MyPiglin.class),
+	PiglinBrute(MyPiglinBrute.class),
+	PigZombie(MyPigZombie.class),
+	Pillager(MyPillager.class),
+	PolarBear(MyPolarBear.class),
+	Pufferfish(MyPufferfish.class),
+	Rabbit(MyRabbit.class),
+	Ravager(MyRavager.class),
+	Salmon(MySalmon.class),
+	Sheep(MySheep.class),
+	Silverfish(MySilverfish.class),
+	Skeleton(MySkeleton.class),
+	SkeletonHorse(MySkeletonHorse.class),
+	Slime(MySlime.class),
+	Snowman(MySnowman.class),
+	Spider(MySpider.class),
+	Squid(MySquid.class),
+	Stray(MyStray.class),
+	Strider(MyStrider.class),
+	TraderLlama(MyTraderLlama.class),
+	TropicalFish(MyTropicalFish.class),
+	Turtle(MyTurtle.class),
+	WanderingTrader(MyWanderingTrader.class),
+	Witch(MyWitch.class),
+	Wither(MyWither.class),
+	WitherSkeleton(MyWitherSkeleton.class),
+	Wolf(MyWolf.class),
+	Vex(MyVex.class),
+	Villager(MyVillager.class),
+	Vindicator(MyVindicator.class),
+	Zoglin(MyZoglin.class),
+	Zombie(MyZombie.class),
+	ZombieHorse(MyZombieHorse.class),
+	ZombieVillager(MyZombieVillager.class),
+	ZombifiedPiglin(MyZombifiedPiglin.class);
 
-    private final Class<? extends MyPet> clazz;
+	private final Class<? extends MyPet> clazz;
 
-    MyPetClass(Class<? extends MyPet> clazz) {
-        this.clazz = clazz;
-    }
+	MyPetClass(Class<? extends MyPet> clazz) {
+		this.clazz = clazz;
+	}
 
-    public static MyPetClass getByMyPetType(MyPetType type) {
-        return valueOf(type.name());
-    }
+	public static MyPetClass getByMyPetType(MyPetType type) {
+		return valueOf(type.name());
+	}
 
-    public Class<? extends MyPet> getMyPetClass() {
-        return clazz;
-    }
+	public Class<? extends MyPet> getMyPetClass() {
+		return clazz;
+	}
 
-    public MyPet getNewMyPetInstance(MyPetPlayer owner) {
-        try {
-            Constructor<?> ctor = clazz.getConstructor(MyPetPlayer.class);
-            Object obj = ctor.newInstance(owner);
-            if (obj instanceof MyPet) {
-                return (MyPet) obj;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public MyPet getNewMyPetInstance(MyPetPlayer owner) {
+		try {
+			Constructor<?> ctor = clazz.getConstructor(MyPetPlayer.class);
+			Object obj = ctor.newInstance(owner);
+			if (obj instanceof MyPet) {
+				return (MyPet) obj;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
